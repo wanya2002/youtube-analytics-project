3578c1b2e43b53a39e6551aa1f60c6171bab8710
wanya2002 <wanya2002@yandex.ru>
2023-04-25 23:59:13 +0300

    new commit1

--- src/channel.py
+++ src/channel.py
@@ -7,11 +7,40 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = self.channel['items'][0]['snippet']['thumbnails']['default']['url']
+        self.subscriber = self.channel['items'][0]['statistics']['subscriberCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         api_key: str = os.getenv('YT_API_KEY')
         youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YT_API_KEY')
+        cls.youtube = build('youtube', 'v3', developerKey=api_key)
+        return cls.youtube
+
+    def to_json (self, name):
+        data = [self.__channel_id, self.title, self.description, self.url, self.subscriber, self.view_count, self.video_count]
+        with open(name, 'w') as f:
+            json.dump(data, f)
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+
+
